bcc6cd3f0116ef1c17620eb56fb29ccd0d4e39ce
Joonsun Ryu <rjs7289@gmail.com>
2025-12-16 16:13:45 +0900

    SSR 렌더링 API 함수 적용

--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -1,21 +1,45 @@
 import SearchableLayout from '@/components/searchable-layout';
 import style from './index.module.css';
 import { ReactNode } from 'react';
-import books from '@/mock/books.json';
 import BookItem from '@/components/book-item';
+import { InferGetServerSidePropsType } from 'next';
+import fetchBooks from '@/lib/fetch-books';
+import fetchRandomBooks from '@/lib/fetch-random-books';
 
-export default function Home() {
+/* getServerSideProps() */
+// 프레임워크 내에서 미리 약속된 함수
+// 사전 렌더링 과정에서 1번만 실행 -> 서버 측에서만 실행
+// window.~~ 메서드 사용 불가
+export const getServerSideProps = async () => {
+  // 컴포넌트보다 먼저 실행 -> 컴포넌트에 필요한 데이터를 불러오는 함수
+  const [allBooks, recommendedBooks] = await Promise.all([
+    fetchBooks(),
+    fetchRandomBooks(),
+  ]);
+
+  return {
+    props: {
+      allBooks,
+      recommendedBooks,
+    },
+  };
+};
+
+export default function Home({
+  allBooks,
+  recommendedBooks,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        {books.map((book) => (
+        {recommendedBooks.map((book) => (
           <BookItem key={book.id} {...book} />
         ))}
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        {books.map((book) => (
+        {allBooks.map((book) => (
           <BookItem key={book.id} {...book} />
         ))}
       </section>
