099a9780e73f50aa0eb1645aeae724029629e1ed
Joonsun Ryu <rjs7289@gmail.com>
2026-01-03 11:51:24 +0900

    리뷰 재검증 구현

--- section05/src/app/book/[id]/page.tsx
+++ section05/src/app/book/[id]/page.tsx
@@ -46,11 +46,10 @@ async function BookDetail({ bookId }: { bookId: string }) {
   );
 }
 
-
-
 async function ReviewList({ bookId }: { bookId: string }) {
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_URL}/review/book/${bookId}`,
+    { next: { tags: [`review-${bookId}`] } },
   );
 
   if (!response.ok) {
