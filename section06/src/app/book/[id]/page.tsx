09a3fa210a0226e21de1ef517390222b8c74f8a7
Joonsun Ryu <rjs7289@gmail.com>
2026-01-06 10:59:04 +0900

    img 태그를 <Image /> 컴포넌트로 교체

--- section06/src/app/book/[id]/page.tsx
+++ section06/src/app/book/[id]/page.tsx
@@ -3,6 +3,7 @@ import style from './page.module.css';
 import { ReviewData } from '@/types';
 import ReviewItem from '@/components/review/review-item';
 import ReviewEditor from '@/components/review/review-editor';
+import Image from 'next/image';
 
 /* generateStaticParams()에 추가되지 않은 id가 입력되면 404 페이지로 이동시킴 */
 // export const dynamicParams = false;  // 기본값은 true
@@ -34,7 +35,12 @@ async function BookDetail({ bookId }: { bookId: string }) {
         className={style.cover_img_container}
         style={{ backgroundImage: `url('${coverImgUrl}')` }}
       >
-        <img src={coverImgUrl} />
+        <Image
+          src={coverImgUrl}
+          width={240}
+          height={300}
+          alt={`도서 ${title}의 표지 이미지`}
+        />
       </div>
       <div className={style.title}>{title}</div>
       <div className={style.subTitle}>{subTitle}</div>
